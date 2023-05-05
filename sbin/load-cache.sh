bc55cdf6ba0e7973dac8a8964975fce69c10983e
Ubuntu <ubuntu@ip-172-31-13-213.eu-central-1.compute.internal>
2023-05-05 08:31:24 +0000

    fit github

--- sbin/load-cache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export ETCDCTL_API=3
-HOST_1=localhost
-ENDPOINTS=$HOST_1:2379
-
-etcdctl --endpoints=$ENDPOINTS put layout_version $1
--- sbin/unpin-cache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -z "$PIXELS_HOME" ]; then
-  echo "ERROR: PIXELS_HOME is not set."
-  exit 1
-fi
-
-echo "Stopping vmtouch..."
-$PIXELS_HOME/bin/stop-vmtouch.sh"
